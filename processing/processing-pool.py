fcbd78917688f750edea832eab330541b46bebee
Alex-GCX <g1242556827@163.com>
2020-05-22 22:06:26 +0800

    'aa'

--- processing/processing-pool.py
+++ processing/processing-pool.py
@@ -11,15 +11,22 @@ def worker(msg):
     print('%s执行结束, 耗时%0.2f' % (end_time - start_time))
 
 
-def main():
-    # 定义进程池,最大进程数为3
-    pool = Pool(3)
-    for i in range(1, 10):
-        # 每次循环将会用空闲出来的子进程去调用目标
-        pool.apply_async(worker, (i, ))
-    pool.close()
-    pool.join()
+# def main():
+#     # 定义进程池,最大进程数为3
+#     pool = Pool(3)
+#     for i in range(1, 10):
+#         # 每次循环将会用空闲出来的子进程去调用目标
+#         pool.apply_async(worker, (i, ))
+#     pool.close()
+#     pool.join()
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
+# 定义进程池,最大进程数为3
+pool = Pool(3)
+for i in range(1, 10):
+    # 每次循环将会用空闲出来的子进程去调用目标
+    pool.apply_async(worker, (i, ))
+pool.close()
+pool.join()
