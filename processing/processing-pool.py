4ef7ff90668cae9bb9b16f820d00eb683b7c8b7d
Alex-GCX <g1242556827@163.com>
2020-05-22 22:48:05 +0800

    'dd'

--- processing/processing-pool.py
+++ processing/processing-pool.py
@@ -1,32 +1,36 @@
 from multiprocessing import Pool
 import time
 import os
+import random
 
 
 def worker(msg):
     start_time = time.time()
-    print('%s开始执行，进程号%d' % (msg, os.getpid()))
-    time.sleep(2)
+    print('----------%s开始执行，进程号%d' % (msg, os.getpid()))
+    time.sleep(random.random())
     end_time = time.time()
-    print('%s执行结束, 耗时%0.2f' % (end_time - start_time))
+    print('%s执行结束, 耗时%0.2f' % (msg, (end_time - start_time)))
+    # 异常测试
+    print('捕获下面的print异常前')
+    try:
+        print(1 + 'end')
+    except Exception as e:
+        print('捕获到异常')
+    print('不捕获下面的print异常')
+    print(1 + 'end')
+    print('不捕获异常后')
 
 
-# def main():
-#     # 定义进程池,最大进程数为3
-#     pool = Pool(3)
-#     for i in range(1, 10):
-#         # 每次循环将会用空闲出来的子进程去调用目标
-#         pool.apply_async(worker, (i, ))
-#     pool.close()
-#     pool.join()
+def main():
+    # 定义进程池,最大进程数为3
+    pool = Pool(3)
+    for i in range(1, 8):
+        # 每次循环将会用空闲出来的子进程去调用目标
+        pool.apply_async(worker, (i, ))
+    pool.close()
+    pool.join()
+    # worker(0)
 
 
-# if __name__ == '__main__':
-#     main()
-# 定义进程池,最大进程数为3
-pool = Pool(3)
-for i in range(1, 10):
-    # 每次循环将会用空闲出来的子进程去调用目标
-    pool.apply_async(worker, (i, ))
-pool.close()
-pool.join()
+if __name__ == '__main__':
+    main()
