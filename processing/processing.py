995dce3e3d6bd717c33162660636e4f4e4344eec
Alex-GCX <g1242556827@163.com>
2020-05-22 13:50:57 +0800

    'processing'

---
+++ processing/processing.py
@@ -0,0 +1,32 @@
+from multiprocessing import Process
+import time
+import os
+
+
+def dancing():
+    print('开始跳舞，进程号：%d' % os.getpid())
+    for i in range(10):
+        print('正在跳舞：。。。。%d' % i)
+        time.sleep(0.5)
+    print('结束跳舞')
+
+
+def singing():
+    print('开始唱歌，进程号：%d' % os.getpid())
+    for i in range(10):
+        print('正在唱歌：。。。。%d' % i)
+        time.sleep(0.5)
+    print('结束唱歌')
+
+
+def main():
+    # 创建对象
+    p1 = Process(target=dancing)
+    p2 = Process(target=singing)
+    # 调用进程
+    p1.start()
+    p2.start()
+
+
+if __name__ == '__main__':
+    main()
